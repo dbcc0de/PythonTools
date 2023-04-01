aaeca01418ce09f18a4ceb33517fa6424d2baab8
dbcc0de <125756539+dbcc0de@users.noreply.github.com>
2023-04-01 17:10:41 +0000

    Added f string capabilities to print a number with two decimal points

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ name = input("Hello, I'm Bot...Bond Bot. What's your name?\n")
 cash = input("It's nice to meet you, " + name + ". \nHow much money do you want to save in a US bond?\n")
 bond_years = input("That's great! Now would you like to invest in a 2-year note, a 10-year note, or a 30-year bond?\nSelect 1 for a 2-year note\nSelect 2 for a 10-year note\nSelect 3 for a 30-year bond\n")
 
+# print(f"{name} + {cash}") 
+# f string helps print different data types 
+
+
 while int(bond_years) <= 3 and int(bond_years)>= 1:
   if bond_years == "1":
-    print("The current rate for 2-year notes is at 5.05%, so after 2 years your initial " + "$" +  str(cash) + " will become:")
-    print((float(cash) * (1 + (.0505)/2)**(2*2)))
+#    print("The current rate for 2-year notes is at 5.05%, so after 2 years your initial " + "$" +  str(cash) + " will become:")
+#    print((float(cash) * (1 + (.0505)/2)**(2*2)))    
+    print(f"${float(cash) * (1 + (.0505)/2)**(2*2):.2f}")
+
   if bond_years == "2":
     print("The current rate for a 10-year note is at 3.97% so after 10 years your initial " + "$" + str(cash) + " will become:")
-    print((float(cash) * (1 + (.0397)/2)**(2*10)))
+#    print((float(cash) * (1 + (.0397)/2)**(2*10)))
+    print(f"${float(cash) * (1 + (.0397)/2)**(2*10):.2f}")
+
   if bond_years == "3":
     print("The current rate for a 30-year bond is 3.88% so your initial " + "$" + str(cash) + " will become :")
     print((float(cash) * (1 + (.0388)/2)**(2*30)))
+    print(f"${float(cash) * (1 + (.0388)/2)**(2*30):.2f}")
   break
 else:
     print("Please try running this code again.\n Input the number 1 for a 2-year note.\n Input the number 2 for a 10-year note.\n Input the number 3 for a 30-year bond.")
